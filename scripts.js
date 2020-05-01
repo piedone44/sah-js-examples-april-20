fd29cccf8c9052741e9a89ea2d033e1f108bbd7f
piedone44 <nightauditorschweiz@gmail.com>
2020-05-01 14:11:21 +0200

    add jquery first samples

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
+// Thrid Part:
+
+$('h1').html('This is done by <em>JavaScript</em>!');
+$('p').css('border',  '1px solid red');
+$('p').css('padding',  '10px');
+$('h2').toggleClass('blue');
+$('h2').toggleClass('blue');
+$('h2').toggleClass('blue');
 console.log('Script file is loaded.');
 
-|   // Single line Comment
-|
-|   /*
+ // Single line Comment
+
+  /*
     Multiple
     line
     of comments
